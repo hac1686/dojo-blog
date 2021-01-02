eb335887f7638fd31e4fb0e30f6c656f56338111
hac1686 <73030989+hac1686@users.noreply.github.com>
2021-01-02 13:24:00 -0500

    add nested components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 //import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() { //has to start with cap letter
-  const title = 'Welcome to the new blog';
-  const likes = 50; //React converts whatever data type to a string before browser output
-  const link = "http://www.google.com";
-
+    
   return (  //have to return this inside a component
     <div className="App">
+      <Navbar />
      <div className="content">
-       <h1>{title}</h1>
-       <p>Likes {likes} times</p>
-       <p>{10}</p>
-       <p>{"hello ninjas"}</p>
-       <p>{[1,2,3,4]}</p>
-       <p>{Math.random() * 10}</p>
-       <a href={link}>Google Site</a>
+          <Home />
+       
      </div>
     </div>
   );
