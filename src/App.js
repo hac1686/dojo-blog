98ada5d3b6bac152726a2e1d54031fe71c98bc46
hac1686 <73030989+hac1686@users.noreply.github.com>
2021-01-02 11:08:50 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() { //has to start with cap letter
   return (
     <div className="App">
       <header className="App-header">
