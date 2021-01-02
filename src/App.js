c7f7cd59017c4c8b211ebe4a25bc5e7d4830d510
hac1686 <73030989+hac1686@users.noreply.github.com>
2021-01-02 12:26:14 -0500

    add dynamic components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@
 import './App.css';
 
 function App() { //has to start with cap letter
+  const title = 'Welcome to the new blog';
+  const likes = 50; //React converts whatever data type to a string before browser output
+  const link = "http://www.google.com";
+
   return (  //have to return this inside a component
     <div className="App">
      <div className="content">
-       <h1>App Component</h1>
+       <h1>{title}</h1>
+       <p>Likes {likes} times</p>
+       <p>{10}</p>
+       <p>{"hello ninjas"}</p>
+       <p>{[1,2,3,4]}</p>
+       <p>{Math.random() * 10}</p>
+       <a href={link}>Google Site</a>
      </div>
     </div>
   );
