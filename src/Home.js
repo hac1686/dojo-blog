baa7bb37185c60b441046e11c10c4e058638e8d8
hac1686 <73030989+hac1686@users.noreply.github.com>
2021-01-04 19:01:55 -0500

    add click events on Home buttons

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+
+    const handleClick = (e) => { //e to see the event info
+        console.log('hello, ninjas',e)
+    }
+
+    const handleClickAgain = (name, e) => { //e to see the event info
+       console.log('hello ' + name, e.target)
+       //console.log('hello ' + name);
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => {
+                handleClickAgain('mario',e) //remove e here and in anonymous funct arg to remove event details
+            }}>Click me again</button>
         </div>
      );
 }
